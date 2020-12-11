08c8a4f142e6878f1a5c32166365e1c398f864e9
jaredkoenig <51336494+jaredkoenig@users.noreply.github.com>
2020-12-10 19:53:10 -0500

    Update waiter.cpp

--- waiter/waiter.cpp
+++ waiter/waiter.cpp
@@ -1,22 +1,40 @@
+/*
+ * waiter.cpp
+ *
+ * Jared Koenig and Emery Weist
+ *
+ * 12/10/2020
+ */
+
 #include <string>
 #include "stdlib.h"
 
 #include "../includes/waiter.h"
+#include "../includes/externs.h"
 
 using namespace std;
 
-Waiter::Waiter(int id,std::string filename):id(id),myIO(filename){
+Waiter::Waiter(int id, std::string filename) :
+		id(id), myIO(filename) {
 }
 
-Waiter::~Waiter()
-{
+Waiter::~Waiter() {
 }
 
-//gets next Order(s) from file_IO
-int Waiter::getNext(ORDER &anOrder){
-	return SUCCESS;
+int Waiter::getNext(ORDER &anOrder) {
+	return myIO.getNext(anOrder);
 }
 
 void Waiter::beWaiter() {
+	ORDER order;
+	while (getNext(order) == SUCCESS) {
+		{
+			lock_guard<mutex> lock(mutex_order_inQ);
+			order_in_Q.push(order);
+			cv_order_inQ.notify_all();
+		}
+	}
+	b_WaiterIsFinished = true;
+	cv_order_inQ.notify_all();
 }
 
